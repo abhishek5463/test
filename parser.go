0e3e2a4fb79f16a2fd408186b2305a48dfa02664
Abhishek <abhishek.ak1012@gmail.com>
2025-09-09 18:31:32 +0530

    updated heap code

--- parser.go
+++ parser.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"os"
-	"sort"
 	"strings"
 )
+
+type WordFreq struct {
+	Word  string
+	Count int
+}
+
+type MinHeap []WordFreq
+
+func (h MinHeap) Len() int            { return len(h) }
+func (h MinHeap) Less(i, j int) bool  { return h[i].Count < h[j].Count } // Min-heap
+func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(WordFreq)) }
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	item := old[n-1]
+	*h = old[:n-1]
+	return item
+}
 
 func Top3Words(filename string) error {
 	file, err := os.Open(filename)
@@ -28,30 +47,28 @@ func Top3Words(filename string) error {
 			}
 		}
 	}
-
 	if err := scanner.Err(); err != nil {
 		return fmt.Errorf("failed to read file: %v", err)
 	}
 
-	type kv struct {
-		Word  string
-		Count int
-	}
-	var wordList []kv
-	for w, c := range wordCount {
-		wordList = append(wordList, kv{w, c})
+	h := &MinHeap{}
+	heap.Init(h)
+
+	for word, count := range wordCount {
+		heap.Push(h, WordFreq{Word: word, Count: count})
+		if h.Len() > 3 {
+			heap.Pop(h) // Keep only top 3
+		}
 	}
 
-	sort.Slice(wordList, func(i, j int) bool {
-		return wordList[i].Count > wordList[j].Count
-	})
+	result := make([]WordFreq, h.Len())
+	for i := len(result) - 1; i >= 0; i-- {
+		result[i] = heap.Pop(h).(WordFreq)
+	}
 
 	fmt.Println("Top 3 most common words:")
-	for i := 0; i < len(wordList) && i < 3; i++ {
-		fmt.Printf("%s: %d\n", wordList[i].Word, wordList[i].Count)
+	for _, wf := range result {
+		fmt.Printf("%s: %d\n", wf.Word, wf.Count)
 	}
-
 	return nil
 }
-
-
